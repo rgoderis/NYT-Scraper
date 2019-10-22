2ac57421403647d1f1f45f30c1735eaafd15f50c
Ryan Goderis <rgoderis789@gmail.com>
2019-10-22 16:27:03 -0400

    created app.js package.json installed npm packages and requred them

---
+++ app.js
@@ -0,0 +1,8 @@
+// dependencies
+const express = require("express");
+const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const axios = require("axios");
+// initialize express
+const app = express();
