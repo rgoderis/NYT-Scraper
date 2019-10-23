56a969972db2d0549dac8cfd076a80dc98539251
Ryan Goderis <rgoderis789@gmail.com>
2019-10-23 16:26:52 -0400

    set scraper to retrieve title text and link for each article on nytimes.com and create a result obj

--- app.js
+++ app.js
@@ -29,4 +29,25 @@ mongoose.connect("mongodb://localhost/nyt_scraper", {useNewUrlParser: true});
 
 // ROUTES
 // route to scrape new data
-app.get("/api/scrape", function(req, res))
+app.get("/api/scrape", function(req, res){
+    // retrieve info from nytimes
+    axios.get("https://www.nytimes.com").then(response=>{
+        const $ = cheerio.load(response.data);
+        // loop over each article and grab data 
+        $("article").each(i, element=>{
+            // save empty result obj
+            let result = {}
+            // add title, text, and href of each link and save as properties of result
+            result.title = $(this)
+            .find("h2")
+            .text()
+            result.text = $(this)
+            .find("p")
+            .text()
+            result.link = $(this)
+            .find("a")
+            .attr("href")
+            console.log(result);
+        })
+    })
+})
