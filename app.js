e62e418f183136546808688595a7c41b6bcbd84b
Ryan Goderis <rgoderis789@gmail.com>
2019-10-23 15:35:48 -0400

    required morgan, created models dir with index, Article, and Note.js, required models in app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // dependencies
 const express = require("express");
-const handlebars = require("express-handlebars");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+
+// scraping tools
 const cheerio = require("cheerio");
 const axios = require("axios");
 // initialize express
 const app = express();
+
+// require all models
+const db = require("./models");
