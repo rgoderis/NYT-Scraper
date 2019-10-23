f54d1f28cdae3000042a4aededefc8c6886e4e7d
Ryan Goderis <rgoderis789@gmail.com>
2019-10-23 15:42:05 -0400

    set middleware

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const mongoose = require("mongoose");
 // scraping tools
 const cheerio = require("cheerio");
 const axios = require("axios");
-// initialize express
-const app = express();
 
 // require all models
 const db = require("./models");
+
+const PORT = 3000;
+
+// initialize express
+const app = express();
+
+// middleware
+// morgan logger for logging requests
+app.use(logger("dev"));
+// parse req.body as JSON
+app.use(express.urlencoded({exteded: true}));
+app.use(express.json());
+// set public as static
+app.use(express.static("public"));
+
+// connection to Mongo DB
+mongoose.connect("mongodb://localhost/nyt_scraper", {useNewUrlParser: true});
+
+// ROUTES
+// route to scrape new data
+app.get("/api/scrape", function(req, res))
